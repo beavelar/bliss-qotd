f2714743d92f476e03103a82f54a430ec688f251
Brian <beavelar.github@gmail.com>
2020-12-29 20:51:04 -0700

    Basic Twitter wireup

--- twitter/src/index.ts
+++ twitter/src/index.ts
@@ -1,12 +1,38 @@
-import express from 'express';
+// import express from 'express';
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
+// app.get('/', (req, res) => {
+//   res.send('The sedulous hyena ate the antelope!');
+// });
 
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+// app.listen(port, () => {
+//   return console.log(`server is listening on ${port}`);
+// });
+
+import Twitter from 'twitter';
+
+// TODO: Figure out how this block can be removed
+const config = {
+
+}
+
+const bot = new Twitter(config);
+
+// Set up your search parameters
+var params = {
+  q: 'd#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+bot.get('search/tweets', params, (err, data, response) => {
+  if (!err) {
+    console.log('Data', data);
+  }
+  else {
+    console.log(err);
+  }
 });
