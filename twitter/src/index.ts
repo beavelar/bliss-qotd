ac1d2198e3d50019cafa8e325a985772a49ef663
Brian <beavelar.github@gmail.com>
2020-12-29 22:06:12 -0700

    Bot tweets hello world at startup

--- twitter/src/index.ts
+++ twitter/src/index.ts
@@ -1,16 +1,3 @@
-// import express from 'express';
-
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('The sedulous hyena ate the antelope!');
-// });
-
-// app.listen(port, () => {
-//   return console.log(`server is listening on ${port}`);
-// });
-
 import dotenv from 'dotenv';
 import Twitter from 'twitter';
 import { validEnvironmentKeys } from './environment-keys/environment-keys';
@@ -18,6 +5,24 @@ import { validEnvironmentKeys } from './environment-keys/environment-keys';
 const env = dotenv.config().parsed;
 
 if (validEnvironmentKeys(env)) {
+  // --------------------------------------------------------------------------------
+  // TODO: Implement express aspect of application
+  //
+  // import express from 'express';
+
+  // const app = express();
+  // const port = 3000;
+
+  // app.get('/', (req, res) => {
+  //   res.send('The sedulous hyena ate the antelope!');
+  // });
+
+  // app.listen(port, () => {
+  //   return console.log(`server is listening on ${port}`);
+  // });
+  //
+  // --------------------------------------------------------------------------------
+
   const config = {
     consumer_key: env.CONSUMER_KEY,
     consumer_secret: env.CONSUMER_SECRET,
@@ -26,23 +31,41 @@ if (validEnvironmentKeys(env)) {
   }
   
   const bot = new Twitter(config);
-  
-  // Set up your search parameters
-  var params = {
-    q: 'd#nodejs',
-    count: 10,
-    result_type: 'recent',
-    lang: 'en'
-  }
-  
-  bot.get('search/tweets', params, (err, data, response) => {
-    if (!err) {
-      console.log('Data', data);
+  const tweet = {
+    status: 'Hello world!'
+  };
+
+  bot.post('statuses/update', tweet, (error, data, response) => {
+    if (!error) {
+      console.log(`Successfully tweeted ${tweet}`); // `
     }
     else {
-      console.log(err);
+      console.error(error);
     }
   });
+
+  // --------------------------------------------------------------------------------
+  // Retrieve tweets
+  // TODO: Implement ability to search previous tweets and compare to expected tweet
+  // 
+  // Set up your search parameters
+  // var params = {
+  //   q: 'd#nodejs',
+  //   count: 10,
+  //   result_type: 'recent',
+  //   lang: 'en'
+  // }
+  
+  // bot.get('statuses/update', params, (err, data, response) => {
+  //   if (!err) {
+  //     console.log('Data', data);
+  //   }
+  //   else {
+  //     console.log(err);
+  //   }
+  // });
+  //
+  // --------------------------------------------------------------------------------
 }
 else {
   console.error('Unable to start, verify required environment variables are filled');
